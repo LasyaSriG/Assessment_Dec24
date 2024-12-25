41f726835f16182d56d4b04ef9ae3d2f8467d9d9
LasyaSriG <163024756+LasyaSriG@users.noreply.github.com>
2024-12-25 19:55:18 +0530

    Update variables.tf

--- Second_Task/modules/instance_group/variables.tf
+++ Second_Task/modules/instance_group/variables.tf
@@ -1,1 +1,40 @@
+variable "group_name" {
+  description = "The name of the instance group and related resources"
+  type        = string
+}
 
+variable "instance_template" {
+  description = "The self link of the instance template to use for the managed instance group"
+  type        = string
+}
+
+variable "region" {
+  description = "The region in which the resources will be created"
+  type        = string
+}
+
+variable "zone" {
+  description = "The zone in which the resources will be created"
+  type        = string
+}
+
+variable "target_size" {
+  description = "The initial number of instances in the managed instance group"
+  type        = number
+}
+
+variable "min_size" {
+  description = "The minimum number of instances in the instance group for autoscaling"
+  type        = number
+}
+
+variable "max_size" {
+  description = "The maximum number of instances in the instance group for autoscaling"
+  type        = number
+}
+
+variable "http_health_check_port" {
+  description = "The port on which the health check should check for availability"
+  type        = number
+  default     = 80
+}
