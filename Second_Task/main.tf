f4c1e7a931aca0206c172ea14caddb266ae4d030
LasyaSriG <163024756+LasyaSriG@users.noreply.github.com>
2024-12-25 19:34:09 +0530

    Update main.tf

--- Second_Task/main.tf
+++ Second_Task/main.tf
@@ -5,43 +5,44 @@ provider "google" {
 
 # VPC Module
 module "vpc" {
-  source         = "./modules/vpc"
-  vpc_name       = "custom-vpc"
-  subnet_name    = "custom-subnet"
-  subnet_region  = var.region
+  source       = "./modules/vpc"
+  vpc_name     = "custom-vpc"
+  subnet_name  = "custom-subnet"
+  subnet_region = var.region
   subnet_ip_cidr = "10.0.0.0/16"
 }
 
 # Service Account Module
 module "service_account" {
-  source         = "./modules/service_account"
-  account_id     = "helloworld-sa"
-  display_name   = "Hello World Service Account"
+  source       = "./modules/service_account"
+  account_id   = "helloworld-sa"
+  display_name = "Hello World Service Account"
 }
 
 # Instance Template Module
 module "instance_template" {
-  source        = "./modules/instance_template"
-  instance_name = "helloworld-instance-template"
-  machine_type  = "e2-micro"
-  network       = module.vpc.subnet_link
+  source               = "./modules/instance_template"
+  instance_name        = "helloworld-instance-template"
+  machine_type         = "e2-micro"
+  network              = module.vpc.subnet_link
   service_account_email = module.service_account.email
 }
 
 # Instance Group Module (with Auto-scaling)
 module "instance_group" {
-  source            = "./modules/instance_group"
-  group_name        = "helloworld-instance-group"
-  region            = var.region
-  instance_template = module.instance_template.self_link
-  target_size       = 2
-  min_size          = 1
-  max_size          = 5
+  source               = "./modules/instance_group"
+  group_name           = "helloworld-instance-group"
+  region               = var.region
+  zone                 = var.zone # Add this line to declare the zone variable
+  instance_template    = module.instance_template.self_link
+  target_size          = 2
+  min_size             = 1
+  max_size             = 5
 }
 
 # Load Balancer Module
 module "load_balancer" {
-  source        = "./modules/load_balancer"
+  source       = "./modules/load_balancer"
   backend_group = module.instance_group.instance_group_link
   health_check  = module.instance_group.health_check_link
 }
