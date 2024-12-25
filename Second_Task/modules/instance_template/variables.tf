6c0da241e50f33e2af067a21bbd8beb51c0b312b
LasyaSriG <163024756+LasyaSriG@users.noreply.github.com>
2024-12-25 19:36:48 +0530

    Update variables.tf

--- Second_Task/modules/instance_template/variables.tf
+++ Second_Task/modules/instance_template/variables.tf
@@ -1,20 +1,28 @@
 variable "instance_name" {
-  description = "Name prefix for the instance template"
+  description = "Name prefix for the instance"
+  type        = string
 }
 
 variable "machine_type" {
-  description = "Machine type for the instance template"
+  description = "The machine type for the instance"
+  type        = string
 }
 
 variable "network" {
-  description = "The subnet link to connect the instances"
+  description = "The subnetwork to attach the instance to"
+  type        = string
 }
 
 variable "service_account_email" {
-  description = "Service account email to attach to the instance"
+  description = "The service account email to use for the instance"
+  type        = string
 }
 
 variable "startup_script" {
-  description = "Startup script to run on the VM"
+  description = "The startup script to run on the instance"
   type        = string
+  default     = <<-EOT
+    #!/bin/bash
+    echo "Hello, World!" > /var/log/startup-script.log
+  EOT
 }
