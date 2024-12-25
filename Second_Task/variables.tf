d11d0e7f6996d77a6abbfe38ebc7956a035df275
LasyaSriG <163024756+LasyaSriG@users.noreply.github.com>
2024-12-25 19:33:37 +0530

    Update variables.tf

--- Second_Task/variables.tf
+++ Second_Task/variables.tf
@@ -1,12 +1,11 @@
-# Variable for the GCP project ID
 variable "project_id" {
-  description = "The GCP project ID"
-  type        = string
+  type = string
 }
 
-# Variable for the region to deploy the resources in
 variable "region" {
-  description = "The region to deploy the resources"
-  type        = string
-  default     = "us-central1"
+  type = string
+}
+
+variable "zone" {
+  type = string
 }
