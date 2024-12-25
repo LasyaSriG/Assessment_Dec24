934e15d1900940a0d3f25a80481ebdc139023096
LasyaSriG <163024756+LasyaSriG@users.noreply.github.com>
2024-12-25 19:52:30 +0530

    Update variables.tf

--- Second_Task/modules/vpc/variables.tf
+++ Second_Task/modules/vpc/variables.tf
@@ -1,1 +1,15 @@
+variable "vpc_name" {
+  description = "Name of the VPC network"
+}
 
+variable "subnet_name" {
+  description = "Name of the subnet"
+}
+
+variable "subnet_region" {
+  description = "Region for the subnet"
+}
+
+variable "subnet_ip_cidr" {
+  description = "IP CIDR range for the subnet"
+}
